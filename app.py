cf3674575f9a5d7b383996dda6ef99731427dc32
AkshithRajkumar <akshithr08@gmail.com>
2025-02-22 20:27:26 -0500

    updating port

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def get_audio():
         return jsonify({"error": "Audio file not found."}), 404      
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8000)
--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start Flask backend
-gunicorn -w 4 -b 0.0.0.0:5000 app:app &
+gunicorn -w 4 -b 0.0.0.0:8000 app:app &
 
 # Start Streamlit frontend
 streamlit run app_frontend.py --server.port 8501 --server.address 0.0.0.0
